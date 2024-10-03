81f2e4b128b77ba4060ccf55a9a8f8226fe42f1f
Deepak verma <dv87178@gmail.com>
2024-10-03 23:31:28 +0530

    v3

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-if(process.env.NODE_ENV!=="production"){
+if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
 }
 
@@ -25,17 +25,18 @@ const wrapAsync = require('./utils/wrapAsync');
 const projectController = require('./controllers/projects')
 const mongoSanitize = require('express-mongo-sanitize');
 
-const dbUrl ='mongodb://localhost:27017/student-project-platform';
 
+
+const dbUrl = process.env.db_url || 'mongodb://localhost:27017/student-project-platform';
 
 // Database Connection
 mongoose.connect(dbUrl).
-then(() => {
-    console.log('connection established successfully')
-})
-.catch((err) => {
-    console.log("error in connection", err);
-})
+    then(() => {
+        console.log('connection established successfully')
+    })
+    .catch((err) => {
+        console.log("error in connection", err);
+    })
 
 
 
@@ -49,11 +50,11 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
     mongoSanitize({
-      replaceWith: '_',
+        replaceWith: '_',
     }),
-  );
+);
 const secret = process.env.SECRET || 'thisistopsecret';
-  const store = MongoStore.create({
+const store = MongoStore.create({
     mongoUrl: dbUrl,
     touchAfter: 24 * 60 * 60,
     crypto: {
@@ -61,21 +62,21 @@ const secret = process.env.SECRET || 'thisistopsecret';
     }
 });
 
-store.on("error",function(e){
-    console.log("session error",e);
+store.on("error", function (e) {
+    console.log("session error", e);
 })
 
 const sessionconfig = {
     store,
-    name:'session',
+    name: 'session',
     secret,
-    resave:false,
-    saveUninitialized:true,
-    cookie:{
-        httpOnly:true,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
         // secure:true,
-        expires:Date.now() + 1000*60*60*24*7,
-        maxAge: 1000*60*60*24*7
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
 
     }
 }
@@ -93,40 +94,40 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req,res,next)=>{
-   
+app.use((req, res, next) => {
+
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
 });
 
-app.get('/',wrapAsync(projectController.index));
+app.get('/', wrapAsync(projectController.index));
 
-app.use('/',usersRoutes);
+app.use('/', usersRoutes);
 
-app.use('/projects',projectsRoutes);
+app.use('/projects', projectsRoutes);
 
-app.use('/projects/:id/reviews',reviewsRoutes);
-
-
+app.use('/projects/:id/reviews', reviewsRoutes);
 
 
 
 
-app.all('*',(req,res,next)=>{
-    next(new ExpressError('Page Not Found',404));
+
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404));
 })
 
-app.use((err,req,res,next)=>{
-    const {statusCode=500} = err;
-    if(!err.message) err.message='Something Went Wrong';
-    res.status(statusCode).render('error',{err});
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Something Went Wrong';
+    res.status(statusCode).render('error', { err });
 
 })
 
 const port = process.env.PORT || 3000;
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`listening on ${port}`)
 })
